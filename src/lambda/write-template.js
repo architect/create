9fd9c81d6c40d10f5a29e08ebc814a8b4cc2cc39
Ryan Block <rblock@gmail.com>
2020-11-19 18:33:38 -0800

    Check for existence of custom template

--- src/lambda/write-template.js
+++ src/lambda/write-template.js
@@ -1,6 +1,9 @@
-let { readFileSync, writeFile } = require('fs')
+let { existsSync, readFileSync, writeFile } = require('fs')
 
 module.exports = function writeTemplate ({ handlerFile, template }, callback) {
+  if (!existsSync(template)) {
+    throw Error(`Custom function template not found: ${template}`)
+  }
   let body = readFileSync(template).toString()
   writeFile(handlerFile, body, callback)
 }
