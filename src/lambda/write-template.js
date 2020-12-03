308128d434edfe0ed6c156b92d6037fbe07da851
Ryan Block <rblock@gmail.com>
2020-12-02 19:10:26 -0800

    Add support for custom templates in paths outside the project directory

--- src/lambda/write-template.js
+++ src/lambda/write-template.js
@@ -1,9 +1,12 @@
 let { existsSync, readFileSync, writeFile } = require('fs')
+let { join, resolve } = require('path')
+let { homedir } = require('os')
 
 module.exports = function writeTemplate ({ handlerFile, template }, callback) {
-  if (!existsSync(template)) {
+  let path = template[0] === '~' ? join(homedir(), template.substr(1)) : resolve(template)
+  if (!existsSync(path)) {
     throw Error(`Custom function template not found: ${template}`)
   }
-  let body = readFileSync(template).toString()
+  let body = readFileSync(path).toString()
   writeFile(handlerFile, body, callback)
 }
