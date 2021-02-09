0f76f0bf678a4ac6ed2651c6064914792138228f
filmaj <maj.fil@gmail.com>
2021-02-09 08:58:19 -0600

    polyfilling Array.flat on nodejs 10

--- src/index.js
+++ src/index.js
@@ -112,4 +112,34 @@ module.exports = async function create (params = {}, callback) {
   return promise
 }
 
+// In order to support nodejs v10
+// Stolen from https://stackoverflow.com/questions/53072385/array-prototype-flat-is-undefined-in-nodejs
+if (!Array.prototype.flat) {
+  Array.prototype.flat = function (maxDepth, currentDepth) {
+    let array = this
+    maxDepth = maxDepth === Infinity
+      ? Number.MAX_SAFE_INTEGER
+      : parseInt(maxDepth, 10) || 1
+    currentDepth = parseInt(currentDepth, 10) || 0
+
+    // It's not an array or it's an empty array, return the object.
+    if (!Array.isArray(array) || !array.length) {
+      return array
+    }
+
+    // If the first element is itself an array and we're not at maxDepth,
+    // flatten it with a recursive call first.
+    // If the first element is not an array, an array with just that element IS the
+    // flattened representation.
+    // **Edge case**: If the first element is an empty element/an "array hole", skip it.
+    // (see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flat#Examples)
+    let firstElemFlattened = (Array.isArray(array[0]) && currentDepth < maxDepth)
+      ? array[0].flat(maxDepth, currentDepth + 1)
+      : array[0] === undefined ? [] : [ array[0] ]
+
+    return firstElemFlattened.concat(array.slice(1).flat(maxDepth, currentDepth))
+  }
+}
+
+
 module.exports.bootstrap = bootstrap
