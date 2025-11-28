c907e705772692fdde98fe544f2bcd76b799e332
Brian LeRoux <brian.leroux@sanity.io>
2025-11-28 10:20:59 -0800

    fix: failing tests hopefully haha

--- test/integration/cli-test.js
+++ test/integration/cli-test.js
@@ -4,7 +4,6 @@ let cli = require('../../src/cli')
 let { join, resolve } = require('path')
 let { readFileSync, existsSync, rmSync, mkdirSync } = require('fs')
 let tmp = resolve(__dirname, '..', 'tmp')
-let origCwd = process.cwd()
 let argv = process.argv
 let args = s => process.argv = [ 'fake-env', 'fake-file', ...s.split(' ') ]
 
@@ -23,64 +22,39 @@ describe('CLI Integration Tests', () => {
   })
 
   beforeEach(() => {
-    // Clean tmp directory before each test but stay in original directory
+    // Clean tmp directory before each test
     emptyDirSync(tmp)
   })
 
   it('should build the basic templated node runtime project', async () => {
-    process.chdir(tmp)
-    try {
-      args('--no-install --runtime node.js')
-      await cli()
-      assert.ok(existsSync(join(tmp, 'src', 'http', 'get-index', 'index.mjs')), 'src/http/get-index/index.mjs created')
-      assert.ok(readFileSync(join(tmp, 'app.arc'), 'utf-8').match(/runtime node/), '"runtime node" present somewhere in manifest')
-    }
-    finally {
-      process.chdir(origCwd)
-    }
+    args(`--no-install --runtime node.js ${tmp}`)
+    await cli()
+    assert.ok(existsSync(join(tmp, 'src', 'http', 'get-index', 'index.mjs')), 'src/http/get-index/index.mjs created')
+    assert.ok(readFileSync(join(tmp, 'app.arc'), 'utf-8').match(/runtime node/), '"runtime node" present somewhere in manifest')
   })
 
   it('should build the basic templated deno runtime project', async () => {
-    process.chdir(tmp)
-    try {
-      args('--no-install --runtime deno')
-      await cli()
-      assert.ok(existsSync(join(tmp, 'src', 'http', 'get-index', 'mod.ts')), 'src/http/get-index/mod.ts created')
-      assert.ok(readFileSync(join(tmp, 'app.arc'), 'utf-8').match(/runtime deno/), '"runtime deno" present somewhere in manifest')
-    }
-    finally {
-      process.chdir(origCwd)
-    }
+    args(`--no-install --runtime deno ${tmp}`)
+    await cli()
+    assert.ok(existsSync(join(tmp, 'src', 'http', 'get-index', 'mod.ts')), 'src/http/get-index/mod.ts created')
+    assert.ok(readFileSync(join(tmp, 'app.arc'), 'utf-8').match(/runtime deno/), '"runtime deno" present somewhere in manifest')
   })
 
   it('should build the basic templated python runtime project', async () => {
-    process.chdir(tmp)
-    try {
-      args('--no-install --runtime python')
-      await cli()
-      assert.ok(existsSync(join(tmp, 'src', 'http', 'get-index', 'lambda.py')), 'src/http/get-index/lambda.py created')
-      assert.ok(readFileSync(join(tmp, 'app.arc'), 'utf-8').match(/runtime python/), '"runtime python" present somewhere in manifest')
-    }
-    finally {
-      process.chdir(origCwd)
-    }
+    args(`--no-install --runtime python ${tmp}`)
+    await cli()
+    assert.ok(existsSync(join(tmp, 'src', 'http', 'get-index', 'lambda.py')), 'src/http/get-index/lambda.py created')
+    assert.ok(readFileSync(join(tmp, 'app.arc'), 'utf-8').match(/runtime python/), '"runtime python" present somewhere in manifest')
   })
 
   it('should build the basic templated ruby runtime project', async () => {
-    process.chdir(tmp)
-    try {
-      args('--no-install --runtime ruby')
-      await cli()
-      assert.ok(existsSync(join(tmp, 'src', 'http', 'get-index', 'lambda.rb')), 'src/http/get-index/lambda.rb created')
-      assert.ok(readFileSync(join(tmp, 'app.arc'), 'utf-8').match(/runtime ruby/), '"runtime ruby" present somewhere in manifest')
-    }
-    finally {
-      process.chdir(origCwd)
-    }
+    args(`--no-install --runtime ruby ${tmp}`)
+    await cli()
+    assert.ok(existsSync(join(tmp, 'src', 'http', 'get-index', 'lambda.rb')), 'src/http/get-index/lambda.rb created')
+    assert.ok(readFileSync(join(tmp, 'app.arc'), 'utf-8').match(/runtime ruby/), '"runtime ruby" present somewhere in manifest')
   })
 
   after(() => {
     process.argv = argv
-    process.chdir(origCwd)
   })
 })
